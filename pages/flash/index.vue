549b164e407e4cdb1c3266e85dd1304b457aac72
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-02-20 14:56:31 +0900

    Update index.vue

--- pages/flash/index.vue
+++ pages/flash/index.vue
@@ -1,16 +1,22 @@
 <template>
-    <section>
-      <h1 class="title">あなたの単語帳</h1>
-      <b-field>
-        <b-input type="text" placeholder="title" v-model="newTitle"></b-input>
-        <button class="button" @click="$store.commit('add', newTitle)">作成</button>
-      </b-field>
-      <ul>
-        <li v-for="(item, index) in list" :key="index" @click="$store.commit('toggle', item)">
-          <nuxt-link to="/flash/card">{{item.title}}</nuxt-link>
-        </li>
-      </ul>
-    </section>
+  <section>
+    <h1 class="title">あなたの単語帳</h1>
+    <b-field>
+      <b-input type="text" placeholder="title" v-model="newTitle"></b-input>
+      <button class="button" @click="$store.commit('add', newTitle)">
+        作成
+      </button>
+    </b-field>
+    <ul>
+      <li
+        v-for="(item, index) in list"
+        :key="index"
+        @click="$store.commit('toggle', item)"
+      >
+        <nuxt-link to="/flash/card">{{ item.title }}</nuxt-link>
+      </li>
+    </ul>
+  </section>
 </template>
 <script>
 export default {
@@ -26,5 +32,4 @@ export default {
   }
 }
 </script>
-<style scoped>
-</style>
+<style scoped></style>
