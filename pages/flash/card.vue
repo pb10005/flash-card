993f1049e7d425f71652f9559d9b200ab71c5441
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-02-20 14:58:10 +0900

    Update card.vue

--- pages/flash/card.vue
+++ pages/flash/card.vue
@@ -4,10 +4,20 @@
     <p>進捗度</p>
     <div class="card">
       <content class="card-content">
-        <b-input v-model="word" placeholder="単語"></b-input>
-        <b-input v-model="description" placeholder="説明"></b-input>
+        <b-input
+          v-model="word"
+          placeholder="単語">
+        </b-input>
+        <b-input
+          v-model="description"
+          placeholder="説明">
+        </b-input>
         <b-input v-model="reminder" placeholder="思い出し方"></b-input>
-        <button class="button" @click="$store.commit('cardList/add', {word: word, description: description, reminder: reminder})">追加</button>
+        <button
+          class="button"
+          @click="$store.commit('cardList/add', {word: word, description: description, reminder: reminder})">
+          追加
+        </button>
       </content>
     </div>
     <button class="button" @click="filter">未達成のみ</button>
