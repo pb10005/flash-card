94fff119a8ede779089f9cb696f90c095a1c39fd
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-02-21 18:47:31 +0000

    Add readall

---
+++ src/cards-read-all.js
@@ -0,0 +1,33 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  console.log("Function `cards-read-all` invoked")
+  return client.query(q.Paginate(q.Match(q.Ref("indexes/all_cards"))))
+  .then((response) => {
+    const todoRefs = response.data
+    console.log("Card refs", todoRefs)
+    console.log(`${todoRefs.length} cards found`)
+    // create new query out of todo refs. http://bit.ly/2LG3MLg
+    const getAllTodoDataQuery = todoRefs.map((ref) => {
+      return q.Get(ref)
+    })
+    // then query the refs
+    return client.query(getAllTodoDataQuery).then((ret) => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(ret)
+      })
+    })
+  }).catch((error) => {
+    console.log("error", error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
