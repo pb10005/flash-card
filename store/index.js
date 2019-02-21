ddbd3df8331c91c1f9cc8bedd1d19c6d00938fb0
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-02-21 19:21:19 +0000

    Maybe fix a bug

--- store/index.js
+++ store/index.js
@@ -26,8 +26,8 @@ export const actions = {
   fetchDecks(context, payload) {
     axios.get('/.netlify/functions/cards-read-all')
       .then(response => {
-        alert(JSON.stringify(response))
-        context.commit('importData', response.map(x => x.data))
+        const list = response.data
+        context.commit('importData', list.map(x => x.data))
       })
   }
 }
