26559c610048a8d8fbb0c6687b4652e2f5a27798
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-02-21 18:13:33 +0000

    Rename env var

---
+++ src/cards-read.js
@@ -0,0 +1,24 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  console.log(`Function 'cards-read' invoked.`)
+  return client.query(q.Get(q.Ref(`224932335654535680`)))
+  .then((response) => {
+    console.log("success", response)
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    })
+  }).catch((error) => {
+    console.log("error", error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
