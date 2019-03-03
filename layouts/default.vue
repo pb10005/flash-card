435cd7f0cf23f5cb65649ddc4c02a73586ac7cbf
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-03-04 02:26:57 +0900

    Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,65 +1,63 @@
 <template>
   <v-app>
-    <v-container fluid full-height>
-      <v-navigation-drawer
-        v-model="drawer"
-        fixed
-        app
-      >
-        <v-list dense>
-          <v-list-tile @click="$router.push('/')">
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>ホーム</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile @click="$router.push('/flash')">
-            <v-list-tile-action>
-              <v-icon>list</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>単語帳</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <div data-netlify-identity-menu />
-      </v-navigation-drawer>
-      <v-toolbar color="indigo" dark fixed app>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer" />
-        <v-toolbar-title>単語帳</v-toolbar-title>
-      </v-toolbar>
-      <v-card flat class="container">
-        <v-layout
-          justify-center
-          align-center
-        >
-          <nuxt />
-        </v-layout>
-        <v-bottom-nav
-          :value="true"
-          fixed
-          dark
-          color="secondary"
-        >
-          <v-btn flat color="teal" @click="$router.push('/')">
-            <span>ホーム</span>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="$router.push('/')">
+          <v-list-tile-action>
             <v-icon>home</v-icon>
-          </v-btn>
-
-          <v-btn flat color="teal" @click="$router.push('/flash')">
-            <span>単語帳</span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>ホーム</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$router.push('/flash')">
+          <v-list-tile-action>
             <v-icon>list</v-icon>
-          </v-btn>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>単語帳</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <div data-netlify-identity-menu />
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>単語帳</v-toolbar-title>
+    </v-toolbar>
+    <v-card flat class="container">
+      <v-layout
+        justify-center
+        align-center
+      >
+        <nuxt />
+      </v-layout>
+      <v-bottom-nav
+        :value="true"
+        fixed
+        dark
+        color="secondary"
+      >
+        <v-btn flat color="teal" @click="$router.push('/')">
+          <span>ホーム</span>
+          <v-icon>home</v-icon>
+        </v-btn>
 
-          <v-btn flat color="teal">
-            <span>設定</span>
-            <v-icon>settings</v-icon>
-          </v-btn>
-        </v-bottom-nav>
-      </v-card>
-    </v-container>
+        <v-btn flat color="teal" @click="$router.push('/flash')">
+          <span>単語帳</span>
+          <v-icon>list</v-icon>
+        </v-btn>
+
+        <v-btn flat color="teal">
+          <span>設定</span>
+          <v-icon>settings</v-icon>
+        </v-btn>
+      </v-bottom-nav>
+    </v-card>
     <!-- include the widget -->
     <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
   </v-app>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,31 @@
 <template>
   <v-container fluid>
-    <h1 class="title">
-      単語帳
-    </h1>
-    <h2 class="title is-4">
+    <h2 class="headline">
       暗記
     </h2>
-    <hr>
-    <article class="content">
+    <v-content>
       <p>
         単語とその説明を登録すると、ランダムな順番で単語を表示し、説明を覚えているか確認できます。
       </p>
-    </article>
-    <h2 class="title is-4">
+    </v-content>
+    <h2 class="headline">
       逆引き
     </h2>
-    <hr>
-    <article class="content">
+    <v-content class="content">
       <p>
         説明のみを表示して、単語を覚えているかチェックすることも可能です。
       </p>
-    </article>
-    <h2 class="title is-4">
+    </v-content>
+    <h2 class="headline">
       フィルター
     </h2>
-    <hr>
-    <article class="content">
+    <v-content>
       <p>
         まだ覚えていない単語だけを表示したり、一度覚えた単語を復習したりできます。
       </p>
-    </article>
+    </v-content>
     <p>
-      Powered by <a href="https://github.com/buefy/buefy"> <s>Buefy</s></a><a>Vuetify</a>
+      Powered by <a>Vuetify</a>
     </p>
   </v-container>
 </template>
